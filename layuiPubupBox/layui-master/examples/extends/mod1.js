7e51146aa185162b48db2564a677d3dce543004f
xiaoxiao <xiaolimei20@163.com>
2020-05-24 22:20:11 +0800

    '2020.5.24.22.20'

---
+++ layuiPubupBox/layui-master/examples/extends/mod1.js
@@ -0,0 +1,11 @@
+
+layui.define(['mod2','test', 'layer'], function(exports){
+  var $ = layui.jquery;
+  
+  console.log(layui.mod2, layui.layer, layui.form)
+
+  
+  exports('mod1', {
+    name: 'mod1'
+  })
+});
---
+++ layuiPubupBox/layui-master/examples/extends/mod2.js
@@ -0,0 +1,9 @@
+
+layui.define(['jquery', 'form'], function(exports){
+  console.log(layui.$);
+  console.log("mode2");
+  
+  exports('mod2', {
+    name: 'mod2'
+  })
+});
