65039036833ed61f7e1a977392deb5aa663fc8b4
xiaoxiao <xiaolimei20@163.com>
2020-06-07 22:37:00 +0800

    '2020.6.7.22.36'

--- layuiPubupBox/layui-master/examples/extends/mod2.js
+++ layuiPubupBox/layui-master/examples/extends/mod2.js
@@ -1,7 +1,7 @@
 
 layui.define(['jquery', 'form'], function(exports){
-  console.log(layui.$);
-  console.log("mode2");
+  //console.log(layui.$);
+  //console.log("mode2");
   
   exports('mod2', {
     name: 'mod2'
